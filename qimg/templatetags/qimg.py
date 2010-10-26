b35a68f826ea9f1f057aeff584b1a0553591661e
Issac Kelly <issac.kelly@gmail.com>
2010-10-25 23:04:16 -0400

    Style Changes

--- qimg/templatetags/qimg.py
+++ qimg/templatetags/qimg.py
@@ -3,22 +3,18 @@ from django.core.urlresolvers import reverse
 register = template.Library()   
     
 @register.simple_tag
-def qimg(dimens, some_class, text=None):
+def qimg(dimens, some_class="", text=""):
     """
     create a qimg based on the tokens given
     """
-    argv = dict()
-    if not text[0]:
-        return QImgNode(argv)
-    else:
-        temp = dimens.split('x')
-        width = temp[0]
-        height = temp[1]
 
-    print argv
-    return u'<img src="%(url)s" width="%(width)s" height="%(height)s" class="%(class)s"/>' % \
-      {'url': reverse('qimg_text', kwargs=(dict({'width':width, 'height':height, 'rtext':text}))), 
-      'width': width,
-      'height': height,
-      'class': some_class
-      }
+    temp = dimens.split("x")
+    width = temp[0]
+    height = temp[1]
+
+    return u'<img src="%(url)s" width="%(width)s" height="%(height)s" class="%(class)s"/>' % {
+        "url": reverse("qimg_text", kwargs=(dict({"width":width, "height":height, "rtext":text}))),
+        "width": width,
+        "height": height,
+        "class": some_class
+    }
--- qimg/views.py
+++ qimg/views.py
@@ -2,32 +2,32 @@
 from django.http import HttpResponse, Http404
 from django.conf import settings
 
+from PIL import Image, ImageFont, ImageDraw
+
 def qimg(request, width=150, height=150, rtext = None):
     """"
     A View that Returns a PNG Image generated using PIL, arbitrary widht and height
     Fits the width and height into the center of the image
     
-    originally baesd on Brad Montgomery's post
+    originally baesd on Brad Montgomery"s post
     http://bradmontgomery.blogspot.com/2008/07/django-generating-image-with-pil.html    
     """
 
-    from PIL import Image, ImageFont, ImageDraw 
-    red = (255,0,0) 
     grey = (204,204,204) 
     white = (255,255,255) 
     default_size = 400;
 
     size = (int(width),int(height))             # size of the image to create
-    im = Image.new('RGB', size) # create the image
+    im = Image.new("RGB", size) # create the image
     draw = ImageDraw.Draw(im)   # create a drawing object that is
     draw.rectangle([0,0,int(width),int(height)],fill=grey)
     
     if not rtext:
-        text = width + 'x' + height # text to draw
+        text = width + "x" + height # text to draw
     else:
         text = rtext
     
-    # Now, we'll do the drawing: 
+    # Now, we"ll do the drawing: 
     font = ImageFont.truetype(settings.QIMG_FONT, default_size)
     size = font.getsize(text)
     font_size = int(default_size * 0.7)
@@ -42,12 +42,12 @@ def qimg(request, width=150, height=150, rtext = None):
     
     text_pos = (((int(width)/2)-(size[0]/2)),(int(height)/2-size[1]*3/8)) # top-left position of our text, centered, 3/8 because of the line-height of the font
     draw.text(text_pos, text, fill=white, font=font)
-    del draw # I'm done drawing so I don't need this anymore
+    del draw # I"m done drawing so I don"t need this anymore
 
     # We need an HttpResponse object with the correct mimetype
     response = HttpResponse(mimetype="image/png")
     # now, we tell the image to save as a PNG to the 
     # provided file-like object
-    im.save(response, 'PNG')
+    im.save(response, "PNG")
 
-    return response # and we're done!
+    return response # and we"re done!
